bb3d64c21f7992ac4a4b5477fbf5e7970f7601b3
Eugene Nesvetaev <dev@nesvet.dev>
2025-02-11 14:22:48 +0400

    fix: According to the new request path match method

--- src/server/Middleware.ts
+++ src/server/Middleware.ts
@@ -22,7 +22,7 @@ export class CookieMiddleware extends ClassMiddleware {
 		super();
 		
 		const {
-			requestRegExp = /^\/cookie\/?\?/
+			path = "/cookie"
 		} = options;
 		
 		this.listeners = { GET: [
